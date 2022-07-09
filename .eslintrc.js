05e935bbb9774fd0e12768ed87f49a53122c90ec
long <hoangphilong2611@gmail.com>
2022-07-10 00:42:09 +0700

    complete design register, login

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   // add your custom rules here
   rules: {
     'vue/multi-word-component-names': ['error', {
-      'ignores': ['Hero', 'default', 'Loading', 'Recommend', 'Header']
-    }]
+      'ignores': ['Hero', 'default', 'Loading', 'Recommend', 'Header', 'Login', 'Register']
+    }],
   },
 }
